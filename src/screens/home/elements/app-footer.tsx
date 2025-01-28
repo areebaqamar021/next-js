703cd65362ba1f1adf113b34b07ec4660ca7a213
areebaqamar021 <areebaqamar021@gmail.com>
2025-01-28 20:14:19 +0500

    update footer component to add horizontal padding for improved layout

--- src/screens/home/elements/app-footer.tsx
+++ src/screens/home/elements/app-footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="border-t border-gray-200">
+    <footer className="border-t border-gray-200 px-32">
       <div className="max-w-[1200px] mx-auto px-4 py-16">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Company Info */}
