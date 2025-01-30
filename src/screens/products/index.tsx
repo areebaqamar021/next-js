4f077939924a3f17e4d6654100027b65b772bce3
areebaqamar021 <areebaqamar021@gmail.com>
2025-01-30 16:51:19 +0500

    style: enhance Products screen layout with background image and text overlay

--- src/screens/products/index.tsx
+++ src/screens/products/index.tsx
@@ -3,23 +3,34 @@ import { useGetProducts } from '@src/apis';
 import { ProductCard } from '@src/components';
 import { Empty, Spin } from 'antd';
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 
 function ProductsScreen() {
     const { data: products, isLoading } = useGetProducts({});
 
     return (
         <div>
-            <Image src="/images/shop-bg.png" width={1500} height={1500} alt=''/>
-            <div className='p-5'>
+            <div className="relative w-full h-[50vh]">
+                <Image
+                    src="/images/shop-bg.png"
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Shop Banner"
+                />
+                <div className="absolute inset-0 flex flex-col items-center justify-center">
+                    <h1 className="text-black text-2xl md:text-4xl font-semibold">Shop</h1>
+                    <p className="text-black font-medium">Home &gt; Shop</p>
+                </div>
+
+            </div>
+
+            <div className="p-5 mt-5">
                 {isLoading ? (
                     <div className="flex justify-center p-10">
                         <Spin />
                     </div>
                 ) : !products?.length ? (
-                    <Empty
-                        description="No products"
-                    />
+                    <Empty description="No products" />
                 ) : (
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                         {products?.map((product) => (
@@ -29,7 +40,7 @@ function ProductsScreen() {
                 )}
             </div>
         </div>
-    )
+    );
 }
 
-export default ProductsScreen
+export default ProductsScreen;
